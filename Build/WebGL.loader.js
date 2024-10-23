e2c5f8bbce1e057b3c93d6c3be99e289f0c74553
Sergej <sgorey@bk.ru>
2024-10-23 22:41:57 +0800

    Update

--- Build/WebGL.loader.js
+++ Build/WebGL.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 88397619,
+    wasmFileSize: 88397829,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
