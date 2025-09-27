655c76d6fd3ed72606c876e60aa5016087ff297a
Sergej <sgorey@bk.ru>
2025-09-27 23:22:28 +0800

    u

--- utility.js
+++
@@ -1,8 +0,0 @@
-function isTelegramWebApp() {
-    try {
-        return window.Telegram.WebApp.initData.length != 0;
-    } catch (error) {
-        console.log("Error in isTelegramWebApp():", error);
-        return false;
-    }
-}
